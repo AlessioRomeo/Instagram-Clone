b5fea71a3df773dd30fc016a8bc0374f390f1e30
alessioromeobensalem <alessioromeo2@gmail.com>
2023-01-06 18:05:21 +0100

    Post section complete (Front End)

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Image from "next/image";
 import styles from '../../styles/Post.module.scss'
 import {HiOutlineDotsHorizontal} from "react-icons/hi";
+import {FaRegComment, FaRegHeart} from "react-icons/fa";
+import {BiBookmark, BiSmile} from "react-icons/bi";
+import {FiSend} from "react-icons/fi";
 
 function Post({id, username, userImage, image, caption}) {
     return (
@@ -12,6 +15,22 @@ function Post({id, username, userImage, image, caption}) {
                     <HiOutlineDotsHorizontal className={styles.headerSettings}/>
             </div>
             <img className={styles.image} src={image} alt="Post Image"/>
+            <div className={styles.buttonSection}>
+                <div className={styles.leftIcons}>
+                    <FaRegHeart className={styles.icon}/>
+                    <FaRegComment className={styles.icon}/>
+                    <FiSend className={styles.icon}/>
+                </div>
+                <BiBookmark className={styles.icon}/>
+            </div>
+            <div className={styles.captionSection}>
+                <h1 className={styles.caption}><span>{username}</span> &nbsp;{caption} </h1>
+            </div>
+            <div className={styles.commentSection}>
+                <BiSmile className={styles.icon}/>
+                <input className={styles.input} placeholder={"Enter your comment..."} type="text"/>
+                <button className={styles.button}>Post</button>
+            </div>
         </div>
     );
 }
